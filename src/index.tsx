c92d59443c23badb3078d38191af74f00d2b1eca
fujisawa <fujisawa5523@gmail.com>
2023-08-14 21:41:39 +0900

    Add redux-devtools-extension for store confirmations

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,11 @@ import App from './App';
 import { createStore } from 'redux';
 import allReducers from './reducers';
 
-const store = createStore(allReducers);
+const store = createStore(
+  allReducers,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
